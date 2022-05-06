d28a239c498816238bb9a67d3de81a8101277a12
BanhsBao <huynhbaofaker@gmail.com>
2022-05-06 21:36:57 +0700

    connection

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const {Payload} = require("./payloads/Payload");
 const {PipeLineHelper} = require("./utils/PipeLineHelper");
 
 const Connection = require("./utils/Connection")
+const con = new Connection('count.intelligentrfid.com.au',6002);
 const cb = (err, res) => {
     // console.log("Error: " + err, res);
 }
--- src/payloads/Payload.js
+++ src/payloads/Payload.js
@@ -1,6 +1,11 @@
 class Payload {
     constructor(connection, cb) {
         this.connection = connection
+        this.host = 'count.intelligentrfid.com.au';
+        this.Port = 6002;
+        this.username = 'LoginUserID';
+        this.password = 'LoginPassword';
+        this.locationCode = 'Location Institution ID';
         this.sequence = 0;
         this.request = {};
         this.response = {};
